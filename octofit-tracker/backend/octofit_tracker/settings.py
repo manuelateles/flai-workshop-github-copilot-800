04ba34476fb071e41e8100cd393d88822d6c343b
Manuela Teles <134373846+manuelateles@users.noreply.github.com>
2026-02-11 16:02:17 +0000

    Update ALLOWED_HOSTS and modify API response URLs in settings and urls

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-w-qay211mgf$@irxf_m4(0_n=u#odc309b5mzn)8p4_hcw^chm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if os.environ.get('CODESPACE_NAME'):
+    ALLOWED_HOSTS.append(f"{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev")
 
 
 # Application definition
